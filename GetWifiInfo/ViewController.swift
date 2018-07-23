dfab49427031faf2df4b0f10c9f084354fa36875
James Baughman <mwcs01@me.com>
2018-07-23 00:40:45 -0500

    Initial Commit

---
+++ GetWifiInfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetWifiInfo
+//
+//  Created by Jim Baughman on 7/23/18.
+//  Copyright © 2018 Midwest Computer Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
